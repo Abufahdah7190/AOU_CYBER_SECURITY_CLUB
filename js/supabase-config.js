ecab5e91e03cf99a440aeb0606ed6f60b6a62c57
Abufahdah7190 <22410669ksa@aou.edu.sa>
2026-09-09 21:47:37 +0300

    Add files via upload

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,2 @@
-const SUPABASE_URL = window.SUPABASE_URL || 'https://znsbsbqsrbjbzhaxjaxe.supabase.co';
-const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'sb_publishable_9lcAuDmyfwAW9l9gupbT8Q_imlN_oci';
-
+const SUPABASE_URL = window.SUPABASE_URL || 'https://mztweayxpiqnymmwzdtd.supabase.co';
+const SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY || 'sb_publishable_-nUUEu1AdSnjb8SzbJ8VTg_K66lzBVe';
